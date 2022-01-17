2f1b7eb96b42df7aa9d9e682d741edb9f35b9359
Arman <armanrajput992@gmail.com>
2022-01-17 15:47:02 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './components/navbar';
+import { Route,Routes } from 'react-router-dom';
+import { Login } from './components/login';
+import {Form} from "./components/home.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+     
+      <Routes>
+        <Route path ="/" element={ <Form/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
